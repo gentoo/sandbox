f7ffcb59df65215209175e0645354f1161e2f3ee
Mike Frysinger <vapier@gentoo.org>
2007-04-19 01:03:28 +0000

    svn2cl: run during `make dist` rather than `./autogen.sh`
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Generate Changelog by default if we in svn repository
-if [[ -d .svn && $1 != -n ]] ; then
-	# For some reason svn do not give the full log if we do not
-	# update first ...
-	svn update
-	./scripts/svn2cl.sh
-fi
+# we'll generate ChangeLog when doing `make dist`
+touch ChangeLog
 
-aclocal-1.9 || exit 1
-libtoolize --automake -c -f || exit 1
-aclocal-1.9 || exit 1
-autoconf || exit 1
-autoheader || exit 1
-automake-1.9 -a -c || exit 1
+autoreconf -i -f
 
 if [[ -x ./test.sh ]] ; then
 	exec ./test.sh "$@"
