373b9bcf101988405419ac4032818b3531f75934
Mike Frysinger <vapier@gentoo.org>
2008-11-16 01:40:28 +0000

    libsbutil: unify duplicate log debug functions
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- libsbutil/get_sandbox_log.c
+++ libsbutil/get_sandbox_log.c
@@ -13,13 +13,13 @@
 #include "headers.h"
 #include "sbutil.h"
 
-void get_sandbox_log(char *path)
+static void _get_sb_log(char *path, const char *env, const char *prefix)
 {
 	char *sandbox_log_env = NULL;
 
 	save_errno();
 
-	sandbox_log_env = getenv(ENV_SANDBOX_LOG);
+	sandbox_log_env = getenv(env);
 
 	/* THIS CHUNK BREAK THINGS BY DOING THIS:
 	 * SANDBOX_LOG=/tmp/sandbox-app-admin/superadduser-1.0.7-11063.log
@@ -29,10 +29,20 @@ void get_sandbox_log(char *path)
 	    sandbox_log_env = NULL;
 
 	snprintf(path, SB_PATH_MAX, "%s%s%s%s%d%s",
-			SANDBOX_LOG_LOCATION, LOG_FILE_PREFIX,
+			SANDBOX_LOG_LOCATION, prefix,
 			(sandbox_log_env == NULL ? "" : sandbox_log_env),
 			(sandbox_log_env == NULL ? "" : "-"),
 			getpid(), LOG_FILE_EXT);
 
 	restore_errno();
+}
+
+void get_sandbox_log(char *path)
+{
+	_get_sb_log(path, ENV_SANDBOX_LOG, LOG_FILE_PREFIX);
+}
+
+void get_sandbox_debug_log(char *path)
+{
+	_get_sb_log(path, ENV_SANDBOX_DEBUG_LOG, DEBUG_LOG_FILE_PREFIX);
 }
