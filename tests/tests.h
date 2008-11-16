73a1f1f082f6f896a362f8176f43ce45691ba091
Mike Frysinger <vapier@gentoo.org>
2008-11-16 06:58:28 +0000

    tests: add errp()
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- tests/tests.h
+++ tests/tests.h
@@ -2,4 +2,6 @@
 
 #define _msg(std, fmt, args...) fprintf(std, "%s:%s():%i: " fmt "\n", __FILE__, __func__, __LINE__, ##args)
 #define _stderr_msg(fmt, args...) _msg(stderr, fmt, ##args)
+#define _stderr_pmsg(fmt, args...) _msg(stderr, fmt ": %s", ##args, strerror(errno))
 #define err(fmt, args...) ({ _stderr_msg(fmt, ##args); exit(1); })
+#define errp(fmt, args...) ({ _stderr_pmsg(fmt, ##args); exit(1); })
