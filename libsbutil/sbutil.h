dddd2da9feeeba54b3b75d345f2f38ba20e7d47a
Mike Frysinger <vapier@gentoo.org>
2009-01-29 11:13:12 -0500

    libsbutil: setup {offsetof,MIN,MAX} helper macros if they dont exist
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- libsbutil/sbutil.h
+++ libsbutil/sbutil.h
@@ -16,7 +16,18 @@
 #include "headers.h"
 #include "include/rcscripts/rcutil.h"
 
+#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
+#endif
+#ifndef MAX
+#define MAX(x, y) ((x) < (y) ? (y) : (x))
+#endif
+#ifndef MIN
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#endif
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
 
 #define SANDBOX_CONF_FILE      ETCDIR "/sandbox.conf"
 #define SANDBOX_CONFD_DIR      ETCDIR "/sandbox.d"
