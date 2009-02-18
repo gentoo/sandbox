35aad022be07f5c8cca4de9a9c71480157275800
Mike Frysinger <vapier@gentoo.org>
2009-02-18 15:17:36 -0500

    include the m4 files from the autoconf-archive package
    Not everyone has the autoconf-archive package installed, so have the
    autogen.sh script automatically update the local copies and include them
    in the tree.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,13 @@
 # we'll generate ChangeLog when doing `make dist`
 touch ChangeLog
 
+# not everyone has sys-devel/autoconf-archive installed
+for macro in $(grep -o '\<AX[A-Z_]*\>' configure.ac | sort -u) ; do
+	m4=$(grep -rl "\[${macro}\]" /usr/share/aclocal/)
+	[[ -z $m4 ]] && continue
+	cp -v $m4 m4/
+done
+
 autoreconf -i -f
 
 if [[ -x ./test.sh ]] ; then
