9e67456ccba9bf8e5c5d37e91c5822f1b63e2f8c
Mike Frysinger <vapier@gentoo.org>
2009-02-18 17:41:40 -0500

    do not fail autogen step if autoconf-archive is not installed
    Rework the shell code slightly so a missed grep does not trigger the -e.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -5,9 +5,9 @@ touch ChangeLog
 
 # not everyone has sys-devel/autoconf-archive installed
 for macro in $(grep -o '\<AX[A-Z_]*\>' configure.ac | sort -u) ; do
-	m4=$(grep -rl "\[${macro}\]" /usr/share/aclocal/)
-	[[ -z $m4 ]] && continue
-	cp -v $m4 m4/
+	if m4=$(grep -rl "\[${macro}\]" /usr/share/aclocal/) ; then
+		cp -v $m4 m4/
+	fi
 done
 
 autoreconf -i -f
