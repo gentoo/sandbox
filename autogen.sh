989600e64b9e3e5ea894e317c50b25c1abfb6cf8
Mike Frysinger <vapier@gentoo.org>
2010-11-15 05:37:43 -0500

    scrub generated m4 files before rebuilding autotools
    When the local m4 code is a diff version of libtool, updating in place
    likes to generate ugly harmless warnings.  But I don't want to see them.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,10 @@
 # we'll generate ChangeLog when doing `make dist`
 touch ChangeLog
 
+# avoid ugly warnings due to mismatch between local libtool and
+# whatever updated version is on the host
+find m4/*.m4 '!' -name 'ax_*.m4' -delete 2>/dev/null
+
 # not everyone has sys-devel/autoconf-archive installed
 for macro in $(grep -o '\<AX[A-Z_]*\>' configure.ac | sort -u) ; do
 	if m4=$(grep -rl "\[${macro}\]" /usr/share/aclocal/) ; then
