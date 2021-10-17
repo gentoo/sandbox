948a4538cee831e7108539d4a427eeef255db3a1
Mike Frysinger <vapier@gentoo.org>
2021-10-16 23:15:49 -0400

    libsandbox: add array bounds to times related funcs
    While we don't care about them, the compiler does and warns about
    mismatches, so add explicit [2] to them.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- libsandbox/wrapper-funcs/utimes.c
+++ libsandbox/wrapper-funcs/utimes.c
@@ -5,7 +5,7 @@
  * Licensed under the GPL-2
  */
 
-#define WRAPPER_ARGS_PROTO const char *filename, const struct timeval times[]
+#define WRAPPER_ARGS_PROTO const char *filename, const struct timeval times[2]
 #define WRAPPER_ARGS filename, times
 #define WRAPPER_SAFE() SB_SAFE(filename)
 #include "__wrapper_simple.c"
