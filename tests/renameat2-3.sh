be55fcb37786f6d29ac3db0c9c98bad5b1de5d94
Mike Frysinger <vapier@gentoo.org>
2021-10-21 22:38:12 -0400

    libsandbox: add renameat2 wrapper
    It's basically renameat at this point as we don't care about the flags.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

---
+++ tests/renameat2-3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# make sure we reject bad renames #612202
+
+addwrite $PWD
+mkdir deny
+adddeny $PWD/deny
+
+touch file
+renameat2-0 -1,EACCES AT_FDCWD file AT_FDCWD deny/file || exit 1
+[ -e file ]
+test -s "${SANDBOX_LOG}"
