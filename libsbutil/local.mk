f4872fb69fe16fc416e4211d12811da61e8738b2
Mike Frysinger <vapier@gentoo.org>
2021-11-05 05:47:58 -0400

    Revert "Force sandbox-internal functions to use 64bit file interface"
    This reverts commit 19c215f245faf9a453e7171bddccc690c03f7b72.
    
    We do not want different LFS interfaces being used in different modules
    as it makes debugging a nightmare when different functions think basic
    structures have different layouts & sizes.
    
    This also doesn't address the LFS issues sandbox has when code still
    crashes in libsandbox itself when checking accesses.
    
    Bug: https://bugs.gentoo.org/681892
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- libsbutil/local.mk
+++ libsbutil/local.mk
@@ -2,7 +2,6 @@ noinst_LTLIBRARIES += %D%/libsbutil.la
 
 %C%_libsbutil_la_CPPFLAGS = \
 	$(AM_CPPFLAGS) \
-	-D_FILE_OFFSET_BITS=64 \
 	-I$(top_srcdir)/%D% \
 	-I$(top_srcdir)/%D%/include
 %C%_libsbutil_la_LDFLAGS = -no-undefined
