378995f8efc182f42c4e553eacb081cd67bb2f2a
Michael Orlitzky <mjo@gentoo.org>
2023-07-01 16:52:34 -0400

    tests: use explicit adddeny() calls in fchmod and fchown tests.
    When running the test suite under portage, the entire build directory
    will be writable because portage adds PORTAGE_TMPDIR to SANDBOX_WRITE
    (thanks floppym). This breaks the tests for these two wrappers, since
    they expect to fail when trying to write above $PWD.
    
    To avoid that, we create a new file to call fchown/fchmod on, and then
    explicitly deny access to it.
    
    Closes: https://bugs.gentoo.org/909445
    Signed-off-by: Michael Orlitzky <mjo@gentoo.org>
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>
    (cherry picked from commit e5032c6b89621db0475e36fb06c2905b6a9c024c)

--- tests/fchmod-1.sh
+++ tests/fchmod-1.sh
@@ -4,11 +4,15 @@
 #
 
 addwrite $PWD
+rm -f deny || exit 1
+touch deny || exit 1
+adddeny $PWD/deny
 
 # The sandbox doesn't log anything when it returns a junk file
 # descriptor? It doesn't look like we can test the contents of
 # sandbox.log here... instead, we just have to count on fchmod
 # failing, which it does if you use O_RDWR, and it *should* if you use
 # O_RDONLY (because that won't stop the change of permissions).
-fchmod-0 $(stat --format='%#04a' ../..) ../.. && exit 1
+fchmod-0 $(stat --format='%#04a' $PWD/deny) $PWD/deny && exit 1
+
 exit 0
