e4f9687b0517a691a82693c3bd772516fee01762
Mike Gilbert <floppym@gentoo.org>
2023-08-05 15:18:53 -0400

    erealpath: use separate buffer for readlink
    Fixes a compiler warning:
    ```
    warning: passing argument 2 to 'restrict'-qualified parameter aliases with argument 1 [-Wrestrict]
    ```
    
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>
    (cherry picked from commit 8c3bc21729c3ad13295b586cd185b2b5da686731)

--- libsandbox/canonicalize.c
+++ libsandbox/canonicalize.c
@@ -102,10 +102,12 @@ erealpath(const char *name, char *resolved)
 				if (lstat64(rpath, &st))
 					break;
 				if (S_ISLNK(st.st_mode)) {
-					ssize_t cnt = readlink(rpath, rpath, SB_PATH_MAX - 1);
+					char buffer[SB_PATH_MAX];
+					ssize_t cnt = readlink(rpath, buffer, SB_PATH_MAX - 1);
 					if (cnt == -1)
 						break;
-					rpath[cnt] = '\0';
+					buffer[cnt] = '\0';
+					strcpy(rpath, buffer);
 					if (p) {
 						size_t bytes_left = strlen(p);
 						if (bytes_left >= SB_PATH_MAX)
