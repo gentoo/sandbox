gentoo/sandbox

commit 8b95a0077380fc24dfd3839a3eec586a0b2609d9

Rework path manipulation code
Drop erealpath, canonicalize, and all of gnulib.

New functions:

sb_abspathat computes an absolute path logically with no symlink
handling.

sb_realpathat uses openat(..., O_PATH) and /proc/self/fd to resolve
paths using the kernel.

Signed-off-by: Mike Gilbert <floppym@gentoo.org>

libsandbox/realpath.c
@@ -1,0 +1,204 @@
+#include <headers.h>
+#include "libsandbox.h"
+#include "wrappers.h"
+
+static ssize_t zreadlink(const char *pathname, char *buf, size_t bufsiz)
+{
+	ssize_t s = readlink(pathname, buf, bufsiz);
+	if (s == bufsiz) {
+		errno = ENAMETOOLONG;
+		return -1;
+	}
+	if (s >= 0)
+		buf[s] = '\0';
+	return s;
+}
+
+static ssize_t zreadlinkat(int dirfd, const char *pathname, char *buf, size_t bufsiz)
+{
+	ssize_t s = readlinkat(dirfd, pathname, buf, bufsiz);
+	if (s == bufsiz) {
+		errno = ENAMETOOLONG;
+		return -1;
+	}
+	if (s >= 0)
+		buf[s] = '\0';
+	return s;
+}
+
+static ssize_t get_fd_path(int fd, char *buf, size_t bufsiz)
+{
+	if (fd == AT_FDCWD)
+		return zreadlink("/proc/self/cwd", buf, bufsiz);
+
+	if (fd < 0) {
+		errno = EBADF;
+		return -1;
+	}
+
+	char fdpath[26];
+	sprintf(fdpath, "/proc/self/fd/%i", fd);
+
+	ssize_t s = zreadlink(fdpath, buf, bufsiz);
+	if (s < 0 && errno == ENOENT)
+		errno = EBADF;
+
+	return s;
+}
+
+bool sb_abspathat(int dirfd, const char *restrict path, char *buf, size_t bufsiz)
+{
+	size_t pathlen = path ? strlen(path) : 0;
+
+	if (pathlen >= bufsiz) {
+		errno = ENAMETOOLONG;
+		return false;
+	}
+
+	if (path && path[0] == '/') {
+		memcpy(buf, path, pathlen + 1);
+		return true;
+	}
+
+	ssize_t s = get_fd_path(dirfd, buf, bufsiz);
+	if (s < 0)
+		return false;
+
+	if (buf[0] != '/' || !path)
+		return true;
+
+	if (s > 1)
+		buf[s++] = '/';
+
+	if (s + pathlen >= bufsiz) {
+		errno = ENAMETOOLONG;
+		return false;
+	}
+
+	memcpy(buf + s, path, pathlen + 1);
+	return true;
+}
+
+static bool chase_linkfd(int linkfd, char *buf, size_t bufsiz)
+{
+	ssize_t llen = get_fd_path(linkfd, buf, bufsiz);
+
+	if (llen < 0)
+		return false;
+
+	if (buf[0] != '/')
+		return true;
+
+	char target[SB_PATH_MAX];
+	ssize_t tlen = zreadlinkat(linkfd, "", target, sizeof(target));
+
+	int i;
+
+	for (i = 40; i > 0 && tlen >= 0; --i) {
+		char *dst = (target[0] == '/' ? buf : strrchr(buf, '/') + 1);
+
+		if (dst < buf)
+			dst = buf;
+
+		if (tlen >= bufsiz - (dst - buf)) {
+			errno = ENAMETOOLONG;
+			return false;
+		}
+
+		memcpy(dst, target, tlen + 1);
+
+		tlen = zreadlink(buf, target, sizeof(target));
+	}
+
+	if (i <= 0) {
+		errno = ELOOP;
+		return false;
+	}
+
+	return true;
+}
+
+static int get_path_fd(int dirfd, const char *restrict path, int atflags, int oflags)
+{
+	if (path == NULL || (path[0] == '\0' && (atflags & AT_EMPTY_PATH)))
+		return dirfd;
+
+	oflags |= O_PATH|O_CLOEXEC;
+	if (atflags & AT_SYMLINK_NOFOLLOW)
+		oflags |= O_NOFOLLOW;
+
+	return sb_unwrapped_openat(dirfd, path, oflags, 0);
+}
+
+bool sb_realpathat(int dirfd, const char *restrict path, char *buf, size_t bufsiz, int flags, bool create)
+{
+	const char *bname = NULL;
+
+	int pathfd = get_path_fd(dirfd, path, flags, 0);
+	if (pathfd != AT_FDCWD && pathfd < 0 && errno == ENOENT) {
+		if (!(flags & AT_SYMLINK_NOFOLLOW)) {
+			pathfd = get_path_fd(dirfd, path, flags, O_NOFOLLOW);
+			if (pathfd == AT_FDCWD || pathfd >= 0) {
+				if (!chase_linkfd(pathfd, buf, bufsiz))
+					return false;
+				path = buf;
+			}
+		}
+
+		if (create) {
+			char *slash = strrchr(path, '/');
+			if (slash) {
+				bname = slash + 1;
+
+				const char *dname;
+
+				if (slash > path) {
+					ptrdiff_t len = slash - path;
+					if (len >= bufsiz) {
+						errno = ENAMETOOLONG;
+						return false;
+					}
+					memcpy(buf, path, len);
+					buf[len] = '\0';
+					dname = buf;
+				}
+				else
+					dname = "/";
+
+				pathfd = get_path_fd(dirfd, dname, 0, O_DIRECTORY);
+			}
+			else {
+				bname = path;
+				pathfd = dirfd;
+			}
+		}
+	}
+
+	if (pathfd != AT_FDCWD && pathfd < 0)
+		return false;
+
+	bool ret = true;
+
+	ssize_t len = get_fd_path(pathfd, buf, bufsiz);
+	if (len < 0)
+		ret = false;
+	else if (buf[0] == '/' && bname && bname[0]) {
+		if (len > 1)
+			buf[len++] = '/';
+		size_t blen = strlen(bname);
+		if (blen >= bufsiz - len) {
+			errno = ENAMETOOLONG;
+			ret = false;
+		}
+		else
+			memcpy(buf + len, bname, blen + 1);
+	}
+
+	if (pathfd != dirfd) {
+		int save_errno = errno;
+		close(pathfd);
+		errno = save_errno;
+	}
+
+	return ret;
+}

Makefile.am
@@ -21,7 +21,6 @@ CLEANFILES =
 DISTCLEANFILES =
 
 EXTRA_DIST = \
-	config.h \
 	headers.h \
 	localdecls.h \
 	ChangeLog.0 \

config.h
@@ -1,9 +1,0 @@
-#pragma once
-/*
- * config.h only exists to make the gnulib code work.
- * The real config lives in sandboxconfig.h.
- */
-#ifdef HAVE_CONFIG_H
-#include <sandboxconfig.h>
-#endif
-#include "libsbutil/gnulib/glue.h"

configure.ac
@@ -149,7 +149,7 @@ AC_C_CONST
 AC_TYPE_UID_T
 AC_TYPE_MODE_T
 AC_TYPE_SIZE_T
-AC_CHECK_DECLS_ONCE([environ, rawmemchr])
+AC_CHECK_DECLS_ONCE([environ])
 AC_CHECK_TYPES([ptrdiff_t])
 AC_CHECK_TYPES([sighandler_t, sig_t, __sighandler_t],,,[#include <signal.h>])
 
@@ -208,7 +208,6 @@ AC_CHECK_FUNCS_ONCE(m4_flatten([
 	prctl
 	process_vm_readv
 	ptrace
-	rawmemchr
 	realpath
 	remove
 	removexattr

headers.h
@@ -167,6 +167,4 @@
 # include "localdecls.h"
 #endif
 
-#include "libsbutil/gnulib/glue.h"
-
 #endif

libsandbox/canonicalize.c
@@ -1,217 +1,0 @@
-/* Return the canonical absolute name of a given file.
-   Copyright (C) 1996-2001, 2002 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
-
-#include "headers.h"
-#include "sbutil.h"
-#include "libsandbox.h"
-
-#ifndef __set_errno
-# define __set_errno(val) errno = (val)
-#endif
-
-/* Return the canonical absolute name of file NAME.  A canonical name
-   does not contain any `.', `..' components nor any repeated path
-   separators ('/') or symlinks.  All path components must exist.  If
-   RESOLVED is null, the result is malloc'd; otherwise, if the
-   canonical name is SB_PATH_MAX chars or more, returns null with `errno'
-   set to ENAMETOOLONG; if the name fits in fewer than SB_PATH_MAX chars,
-   returns the name in RESOLVED.  If the name cannot be resolved and
-   RESOLVED is non-NULL, it contains the path of the first component
-   that cannot be resolved.  If the path can be resolved, RESOLVED
-   holds the same value as the value returned.	*/
-
-/* Modified: 19 Aug 2002; Martin Schlemmer <azarah@gentoo.org>
- *
- *  Cleaned up unneeded stuff, and change so that it will not
- *  resolve symlinks.  Also prepended a 'e' to functions that
- *  I did not rip out.
- *
- */
-
-char *
-erealpath(const char *name, char *resolved)
-{
-	char *rpath, *dest, *recover;
-	const char *start, *end, *rpath_limit;
-
-	if (name == NULL) {
-		/* As per Single Unix Specification V2 we must return an error if
-		   either parameter is a null pointer.	We extend this to allow
-		   the RESOLVED parameter to be NULL in case the we are expected to
-		   allocate the room for the return value.  */
-		__set_errno(EINVAL);
-		return NULL;
-	}
-
-	if (name[0] == '\0') {
-		/* As per Single Unix Specification V2 we must return an error if
-		   the name argument points to an empty string.  */
-		__set_errno(ENOENT);
-		return NULL;
-	}
-
-	if (resolved == NULL) {
-		rpath = xmalloc(SB_PATH_MAX);
-	} else {
-		/* We can't handle resolving a buffer inline, so demand
-		 * separate read and write strings.
-		 */
-		sb_assert(name != resolved);
-		rpath = resolved;
-	}
-	rpath_limit = rpath + SB_PATH_MAX;
-
-	recover = NULL;
-	if (name[0] != '/') {
-		if (!egetcwd(rpath, SB_PATH_MAX)) {
-			rpath[0] = '\0';
-			goto error;
-		}
-
-		/* This stat business uses relative paths atm. */
-		if (trace_pid)
-			goto no_recover;
-
-		/* Can we actually access the working dir (sane perms) ?
-		 * If not, try a little harder to consume this path in
-		 * case it has symlinks out into a better world ...
-		 */
-		struct stat st;
-		if (lstat(rpath, &st) == -1 && errno == EACCES) {
-			char *p = rpath;
-			strcpy(rpath, name);
-			do {
-				p = strchr(p, '/');
-				if (p) *p = '\0';
-				if (lstat(rpath, &st))
-					break;
-				if (S_ISLNK(st.st_mode)) {
-					char buffer[SB_PATH_MAX];
-					ssize_t cnt = readlink(rpath, buffer, SB_PATH_MAX - 1);
-					if (cnt == -1)
-						break;
-					buffer[cnt] = '\0';
-					strcpy(rpath, buffer);
-					if (p) {
-						size_t bytes_left = strlen(p);
-						if (bytes_left >= SB_PATH_MAX)
-							break;
-						strncat(rpath, name + (p - rpath + 1),
-							SB_PATH_MAX - bytes_left - 1);
-					}
-
-					/* Ok, we have a chance at something better.  If
-					 * this fails, we give up.  Otherwise we set things
-					 * to match the new paths that we've found and hook
-					 * back into the normal process.
-					 */
-					recover = erealpath(rpath, NULL);
-					if (recover) {
-						if (recover[0] != '/') {
-							strcpy(rpath, recover);
-							break;
-						} else {
-							name = recover;
-							goto recover_full;
-						}
-					} else
-						goto error;
-				}
-				if (p) *p++ = '/';
-			} while (p);
-		}
-
- no_recover:
-		dest = strchr(rpath, '\0');
-	} else {
- recover_full:
-		rpath[0] = '/';
-		dest = rpath + 1;
-	}
-
-	for (start = end = name; *start; start = end) {
-		/* Skip sequence of multiple path-separators.  */
-		while (*start == '/')
-			++start;
-
-		/* Find end of path component.	*/
-		for (end = start; *end && *end != '/'; ++end)
-			/* Nothing.  */ ;
-
-		if (end - start == 0)
-			break;
-		else if (end - start == 1 && start[0] == '.')
-			/* nothing */ ;
-		else if (end - start == 2 && start[0] == '.' && start[1] == '.') {
-			/* Back up to previous component, ignore if at root already.  */
-			if (dest > rpath + 1)
-				while (dest > rpath && (--dest)[-1] != '/')
-					continue;
-		} else {
-			size_t new_size;
-
-			if (dest[-1] != '/')
-				*dest++ = '/';
-
-			if (dest + (end - start) >= rpath_limit) {
-				ptrdiff_t dest_offset = dest - rpath;
-				char *new_rpath;
-
-				if (resolved) {
-					__set_errno(ENAMETOOLONG);
-					if (dest > rpath + 1)
-						dest--;
-					*dest = '\0';
-					goto error;
-				}
-				new_size = rpath_limit - rpath;
-				if (end - start + 1 > SB_PATH_MAX)
-					new_size += end - start + 1;
-				else
-					new_size += SB_PATH_MAX;
-				new_rpath = (char *) xrealloc(rpath, new_size);
-				rpath = new_rpath;
-				rpath_limit = rpath + new_size;
-
-				dest = rpath + dest_offset;
-			}
-
-			memcpy(dest, start, end - start);
-			dest += end - start;
-			*dest = '\0';
-		}
-	}
-#if 1
-	if (dest > rpath + 1 && dest[-1] == '/')
-		--dest;
-#endif
-	*dest = '\0';
-
-	return rpath;
-
-error:
-	if (resolved)
-		snprintf(resolved, SB_PATH_MAX, "%s", rpath);
-	else
-		free(rpath);
-	free(recover);
-	return NULL;
-}
-
-// vim:noexpandtab noai:cindent ai

libsandbox/libsandbox.h
@@ -91,11 +91,9 @@ extern void sb_unlock(void);
 bool trace_possible(const char *filename, char *const argv[], const void *data);
 void trace_main(void);
 
-/* glibc modified realpath() function */
-char *erealpath(const char *, char *);
-char *egetcwd(char *, size_t);
-int canonicalize(const char *, char *);
-int resolve_dirfd_path(int, const char *, char *, size_t);
+bool sb_abspathat(int dirfd, const char *restrict path, char *buf, size_t bufsiz);
+bool sb_realpathat(int dirfd, const char *restrict path, char *buf, size_t bufsiz, int flags, bool create);
+
 /* most linux systems use ENAMETOOLONG, but some (ia64) use ERANGE, as do some BSDs */
 #define errno_is_too_long() (errno == ENAMETOOLONG || errno == ERANGE)
 

commit 92f01200a1fdb159f343fe06a15899394929be7d

libsandbox: do not leak the symlink fd in sb_realpathat()
When a to-be-created target did not exist, sb_realpathat() reopened it
with O_NOFOLLOW to chase the symlink and then reused |pathfd| for the
descriptor of the directory the basename lives in.  The one from the
O_NOFOLLOW open was never closed: the create branch overwrote it, and
the chase_linkfd() failure return walked past the close at the end of
the function.  Every create through a dangling symlink therefore leaked
a descriptor, and under trace mode those accumulated in the tracer until
get_pid_fd() could no longer open anything and check_syscall() started
letting syscalls through unchecked.

Fixes: 8b95a0077380fc24dfd3839a3eec586a0b2609d9 (introduced in v2.44)
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

libsandbox/realpath.c
@@ -139,6 +139,17 @@ static int libc_close(int fd)
 	return close(fd);
 }
 
+static void close_path_fd(int fd, int dirfd)
+{
+	if (fd < 0 || fd == dirfd)
+		return;
+
+	int save_errno = errno;
+	/* Avoid calling close() from libsocket_wrapper, bug 961961 */
+	libc_close(fd);
+	errno = save_errno;
+}
+
 bool sb_realpathat(int dirfd, const char *restrict path, char *buf, size_t bufsiz, int flags, bool create)
 {
 	const char *bname = NULL;
@@ -153,13 +164,18 @@ bool sb_realpathat(int dirfd, const char *restrict path, char *buf, size_t bufsi
 				if (!link)
 					link = alloca(PATH_MAX);
 
-				if (!chase_linkfd(pathfd, link, MIN(bufsiz, PATH_MAX)))
+				if (!chase_linkfd(pathfd, link, MIN(bufsiz, PATH_MAX))) {
+					close_path_fd(pathfd, dirfd);
 					return false;
+				}
 				path = link;
 			}
 		}
 
 		if (create) {
+			close_path_fd(pathfd, dirfd);
+			pathfd = -1;
+
 			const char *slash = strrchr(path, '/');
 			if (slash) {
 				bname = slash + 1;
@@ -208,12 +224,7 @@ bool sb_realpathat(int dirfd, const char *restrict path, char *buf, size_t bufsi
 			memcpy(buf + len, bname, blen + 1);
 	}
 
-	if (pathfd != dirfd) {
-		int save_errno = errno;
-		/* Avoid calling close() from libsocket_wrapper, bug 961961 */
-		libc_close(pathfd);
-		errno = save_errno;
-	}
+	close_path_fd(pathfd, dirfd);
 
 	return ret;
 }

tests/open_static-3.sh
@@ -1,0 +1,25 @@
+#!/bin/sh
+# make sure canonicalizing a dangling symlink does not leak the tracer's fd
+[ "${at_xfail}" = "yes" ] && exit 77 # see trace-0
+
+addwrite $PWD
+mkdir deny || exit 1
+adddeny $PWD/deny
+
+# stays dangling however often it is opened, as nodir/ never shows up
+ln -s nodir/tgt dangle || exit 1
+
+# keep the loop below well over this so the tracer only runs out of
+# descriptors if it leaks one per check
+ulimit -n 128 || exit 1
+
+set --
+i=0
+while [ $i -lt 300 ] ; do
+	set -- "$@" -1,ENOENT dangle "O_WRONLY|O_CREAT" 0666
+	i=$((i + 1))
+done
+
+# the write has to stay denied no matter how many checks came before it
+open_static-0 "$@" -1,EPERM deny/not-ok "O_WRONLY|O_CREAT" 0666 || exit 1
+test ! -e deny/not-ok

tests/open_static.at
@@ -1,2 +1,3 @@
 SB_CHECK(1)
 SB_CHECK(2)
+SB_CHECK(3)